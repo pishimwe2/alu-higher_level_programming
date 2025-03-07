beff0a5fa160837fa7661178cf148ad17a55d11a
pishimwe2 <p.ishimwe2@alustudent.com>
2025-03-07 04:03:23 -0800

    Class has been written

---
+++ python-classes/0-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module defines an empty class called Square.
+The class does not have any attributes or methods.
+"""
+
+
+class Square:
+    """Defines an empty class Square."""
+    pass
