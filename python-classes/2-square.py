8ea46a8172452f25469a919810f3da49a46b1cd7
pishimwe2 <p.ishimwe2@alustudent.com>
2025-03-07 04:14:38 -0800

    Class has been written

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+"""This module defines a class Square with a private size attribute.
+It includes validation to ensure that size is an integer and is greater than or
+equal to 0.
+"""
+
+
+class Square:
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size=0):
+        """Initializes the square with a given size, which defaults to 0."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
