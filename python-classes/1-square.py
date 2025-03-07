bf01a3c6d51e29fe97a23673ba0d0bc4841835f5
pishimwe2 <p.ishimwe2@alustudent.com>
2025-03-07 04:13:24 -0800

    Class has been written

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+"""This module defines a class Square with a private size attribute."""
+
+
+class Square:
+    """Defines a square with a private size attribute."""
+
+    def __init__(self, size):
+        """Initializes a new square with a given size."""
+        self.__size = size
