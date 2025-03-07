39ec54b4d9cf7bf3c6f454df52cbde8d7c68ee3b
pishimwe2 <p.ishimwe2@alustudent.com>
2025-03-07 07:19:25 -0800

    Class has been written

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This module defines a class Rectangle.
+The class is empty and serves as a starting point for future extensions
+where properties and methods for the rectangle can be added.
+"""
+
+
+class Rectangle:
+    """
+    A class to represent a rectangle.
+
+    This class is currently empty but can be expanded in the future
+    to include properties like width, height, area, perimeter, etc.
+    """
+    pass
